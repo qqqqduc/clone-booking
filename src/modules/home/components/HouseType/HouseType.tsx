775966d6cf592cec071ff4e405522da9400eab67
qqqqduc <duclun361@gmail.com>
2024-08-06 15:23:29 +0700

    fix css HouseType component

--- src/modules/home/components/HouseType/HouseType.tsx
+++ src/modules/home/components/HouseType/HouseType.tsx
@@ -12,22 +12,24 @@ interface IProps {
 
 function HouseType({ width, priceElement }: IProps) {
     return (
-        <Card
-            extra={<ICHeart fillColor="#333" />}
-            style={{ width: width }}
-            cover={
-                <img
-                    alt="example"
-                    src="https://cf.bstatic.com/xdata/images/hotel/square600/573101946.webp?k=5d95af7b1c22dc3d7e83cb919332b56aeba4fda4f856b00957853fc5361a0c13&o="
-                />
-            }
-            className="custom-antd-card mx-2"
-        >
-            <Meta title="Home stay in Ha Nam" description="Phủ Lý" />
-            <HouseQuantity />
-            
-            {priceElement}
-        </Card>
+        <a href="#" className="inline-block">
+            <Card
+                extra={<ICHeart fillColor="#333" />}
+                style={{ width: width }}
+                cover={
+                    <img
+                        alt="example"
+                        src="https://cf.bstatic.com/xdata/images/hotel/square600/573101946.webp?k=5d95af7b1c22dc3d7e83cb919332b56aeba4fda4f856b00957853fc5361a0c13&o="
+                    />
+                }
+                className="custom-antd-card mx-2"
+            >
+                <Meta title="Home stay in Ha Nam" description="Phủ Lý" />
+                <HouseQuantity />
+
+                {priceElement}
+            </Card>
+        </a>
     );
 }
 
--- src/modules/home/components/RestAreaSlider/RestAreaType/RestAreaType.tsx
+++ src/modules/home/components/RestAreaSlider/RestAreaType/RestAreaType.tsx
@@ -8,7 +8,7 @@ interface IProps {
 
 function RestAreaType({ name, description }: IProps) {
     return (
-        <div className="house-item d-block">
+        <a href="#" className="house-item">
             <div className="rounded-lg overflow-hidden">
                 <img
                     src="https://q-xx.bstatic.com/xdata/images/xphoto/263x210/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o="
@@ -18,7 +18,7 @@ function RestAreaType({ name, description }: IProps) {
             </div>
             <div className="mt-2 text-base font-bold">title</div>
             <div className="rest-area-type-description">mô tả</div>
-        </div>
+        </a>
     );
 }
 
